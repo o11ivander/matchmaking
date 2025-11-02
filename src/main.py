2adb0a29c3b393ce57e84b292db4615fedfd2916
o11ivander <muzichenko2001@gmail.com>
2025-11-02 14:18:50 +0200

    task2: (#2)
    Add task2:
    - div to match and teams with group_id;

--- src/main.py
+++ src/main.py
@@ -1,14 +1,28 @@
-from data import players
+from data import players_first, players_second
 from schema import Player
 from task1 import form_matches, split_match_to_teams
+from task2 import make_matches_with_squads
 
 if __name__ == "__main__":
 
-    #For first task:
-    players = [Player(**data) for data in players]
+    # For first task:
+    players = [Player(**data) for data in players_first]
     players_matches, players_without_matches = form_matches(players=players)
     for cnt_match, player_match in enumerate(players_matches, 1):
         team1, team2 = split_match_to_teams(player_match)
+        print(f"Match {cnt_match}:")
+        print("Team 1: ", team1)
+        print("Team 2: ", team2)
+    print(f"Players without match[{len(players_without_matches)}]:")
+    print(players_without_matches)
+
+    # For second task:
+    players = [Player(**data) for data in players_second]
+    matches, leftover = make_matches_with_squads(
+        players=players,
+    )
+    for cnt_match, player_match in enumerate(matches, 1):
+        team1, team2 = player_match
         print(f"Match {cnt_match}:")
         print("Team 1: ", team1)
         print("Team 2: ", team2)
--- src/schema.py
+++ src/schema.py
@@ -6,11 +6,19 @@ from typing import Optional
 class Player:
     id: str
     skill: Optional[int] = 0
-    group: Optional[str] = None
+    squad_id: Optional[int] = None
 
     def __str__(self):
         return (
-            f"Player(id): [{self.id}] | skill: {self.skill} | group(id): [{self.group}]"
-            if self.group is not None
+            f"Player(id): [{self.id}] | skill: {self.skill} | group(id): [{self.squad_id}]"
+            if self.squad_id is not None
             else f"Player(id): [{self.id}] | skill: {self.skill}"
         )
+
+
+@dataclass
+class Squad:
+    avg_skill: float
+    id: int
+    cnt_players: int
+    players: list[Player]
